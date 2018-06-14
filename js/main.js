39587ff256428977c983b80d415000b7b6e03c9a
Daniel Delgado <dedd1993@gmail.com>
2018-06-13 23:46:07 -0500

    gulp dependencies removed and scrollify plugin added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
 console.log("Hola mundo");
+
+$(function() {
+    $.scrollify({
+        section : ".section-scroll",
+        sectionName : "section-name",
+        interstitialSection : "",
+        easing: "easeOutExpo",
+        scrollSpeed: 1100,
+        offset : 0,
+        scrollbars: false,
+        standardScrollElements: ".skills",
+        setHeights: true,
+        overflowScroll: true,
+        updateHash: false,
+        touchScroll:true,
+        before:function() {},
+        after:function() {},
+        afterResize:function() {},
+        afterRender:function() {}
+    });
+
+    $('#go').click(function(e){
+        console.log("jajaja")
+        console.log('$.scrollify.next()', $.scrollify.next())
+    });
+});
+
+
+
